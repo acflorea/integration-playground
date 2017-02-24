aac0953a2dc6aae3a2bdc003a3c46706366298f9
Adrian Florea <acflorea@gmail.com>
2017-02-24 16:25:02 +0200

    Deply service as WAR, huge timeout

--- ws/src/main/java/org/acf/RestTest.java
+++ ws/src/main/java/org/acf/RestTest.java
@@ -25,7 +25,7 @@ public class RestTest {
 		}
 
 		try {
-			Thread.sleep(1000);
+			Thread.sleep(3600000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
--- ws/src/main/java/org/acf/WebServerConfig.java
+++ ws/src/main/java/org/acf/WebServerConfig.java
@@ -2,13 +2,20 @@ package org.acf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * @author aflorea.
  */
 
 @SpringBootApplication
-public class WebServerConfig {
+public class WebServerConfig extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WebServerConfig.class);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(WebServerConfig.class, args);
 	}
